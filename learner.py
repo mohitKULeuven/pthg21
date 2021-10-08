0f39cc2168a4315c60ff6f657b0c132fb093817b
Mohit Kumar <mohitkrg@gmail.com>
2021-10-08 22:12:13 +0200

    constraint learner using all solutions. debugging to find unsat core

--- learner.py
+++ learner.py
@@ -1,171 +1,192 @@
 
 import itertools as it
-from sympy import symbols
+from sympy import symbols, lambdify, sympify
 import json
-# from cpmpy import *
-# from cpmpy.solvers import CPM_ortools
+import numpy as np
+from cpmpy import *
+from musx import musx
+from cpmpy.solvers import CPM_ortools
 
 def pairs(example):
     for [x, y] in it.product(example, repeat=2):
         yield x,y
+
+def index_pairs(exampleLen):
+    for (i, j) in it.combinations(range(exampleLen), r=2):
+        yield i, j
 
 
 def unary_operators():
     def modulo(x):
         return x % 2
 
-    def power(x):
-        return x*x
+    # def power(x):
+    #     return x*x
 
     def identity(x):
         return x
 
-    for f in [identity, abs, power, modulo]:
+    for f in [identity, abs, modulo]:
         yield f
 
 def generate_unary_exp(x):
     for u in unary_operators():
         yield u(x)
 
-def generate_binary_expr(x,y):
+def binary_operators(x,y):
     for u in unary_operators():
         yield u(x) + u(y)
         yield u(x) - u(y)
         yield u(y) - u(x)
 
-def generate_expressions(x, y):
-    for u in generate_unary_exp(x):
-        yield u
-
-    for b in generate_binary_expr(x,y):
+def generate_binary_expr(x, y):
+    for b in binary_operators(x,y):
         yield b
 
-    for b in generate_binary_expr(x,y):
-        for u in generate_unary_exp(b):
+    for b in binary_operators(x,y):
+        for u in generate_unary_exp(b): #redundancy due to identity
             yield u
 
-def constraint_learner(instance):
-    lb= {}
-    ub = {}
+def constraint_learner(solutions, n_vars):
+    bounds = dict()
     x, y = symbols('x y')
     for u in generate_unary_exp(x):
-        if str(u) not in lb:
-            lb[str(u)]=[]
-            ub[str(u)]=[]
-        for v in instance:
-            val = u.subs({x: v})
-            lb[str(u)].append(val)
-            ub[str(u)].append(val)
+        k = str(u)
+        bounds[k]=dict()
+        f = lambdify(x, u, "math")
+        for i in range(n_vars):
+            vals = f(solutions[:, i])
+            bounds[k][(i,)] = (min(vals), max(vals))
 
-    for b in generate_binary_expr(x,y):
-        if str(b) not in lb:
-            lb[str(b)] = []
-            ub[str(b)] = []
-        for (v1,v2) in pairs(instance):
-            val=b.subs({x:v1, y: v2})
-            lb[str(b)].append(val)
-            ub[str(b)].append(val)
+    for b in generate_binary_expr(x, y):
+        k = str(b)
+        bounds[k] = dict()
+        f = lambdify([x,y], b, "math")
+        for (i,j) in index_pairs(n_vars):
+            vals = f(solutions[:, i], solutions[:, j])
+            bounds[k][(i,j)] = (min(vals), max(vals))
+    return bounds
 
+def filter_negatives(negData, lb, ub):
+    x, y = symbols('x y')
     for u in generate_unary_exp(x):
-        for b in generate_binary_expr(x,y):
-            k=str(u)
-            k=k.replace('x', '('+str(b)+')')
-            if k not in lb:
-                lb[k] = []
-                ub[k] = []
-            for (v1, v2) in pairs(instance):
-                val = u.subs({x: b.subs({x: v1, y: v2})})
-                lb[k].append(val)
-                ub[k].append(val)
-    return lb,ub
+        k=str(u)
 
-# def filter_negatives(negData, unaryExpressions, binaryExpressions, lb, ub):
-#     for i,u in enumerate(unaryExpressions):
-#         k=str(i)+","
-#         breaksLB=0
-#         for example in negData:
-#             for v in example:
-#                 if u(v)<lb[k]:
-#                     breaksLB=1
-#                     break
-#             if breaksLB==1:
-#                 break
-#         if breaksLB==0:
-#             del lb[k]
-#
-#         breaksUB = 0
-#         for example in negData:
-#             for v in example:
-#                 if u(v) > ub[k]:
-#                     breaksUB = 1
-#                     break
-#             if breaksUB == 1:
-#                 break
-#         if breaksUB == 0:
-#             del ub[k]
-#
-#     for i,b in enumerate(binaryExpressions):
-#         k=","+str(i)
-#         breaksLB=0
-#         for example in negData:
-#             for (x, y) in pairs(example):
-#                 if b.subs({x:x, y: y})<lb[k]:
-#                     breaksLB=1
-#                     break
-#             if breaksLB==1:
-#                 break
-#         if breaksLB==0:
-#             del lb[k]
-#
-#         breaksUB = 0
-#         for example in negData:
-#             for (x, y) in pairs(example):
-#                 if b.subs({x:x, y: y})>ub[k]:
-#                     breaksUB = 1
-#                     break
-#             if breaksUB == 1:
-#                 break
-#         if breaksUB == 0:
-#             del ub[k]
-#
-#     for i, u in enumerate(unaryExpressions):
-#         for j, b in enumerate(binaryExpressions):
-#             k = str(i)+","+str(j)
-#             breaksLB = 0
-#             for example in negData:
-#                 for (x, y) in pairs(example):
-#                     if u(b.subs({x:x, y: y})) < lb[k]:
-#                         breaksLB = 1
-#                         break
-#                 if breaksLB == 1:
-#                     break
-#             if breaksLB == 0:
-#                 del lb[k]
-#
-#             breaksUB = 0
-#             for example in negData:
-#                 for (x, y) in pairs(example):
-#                     if u(b.subs({x:x, y: y})) > ub[k]:
-#                         breaksUB = 1
-#                         break
-#                 if breaksUB == 1:
-#                     break
-#             if breaksUB == 0:
-#                 del ub[k]
-#     return lb,ub
+        for i in range(len(lb[k])):
+            breaksLB = 0
+            for example in negData:
+                if u.subs({x: example[i]})<lb[k][i]:
+                    breaksLB=1
+                    break
+            if breaksLB==0:
+                del lb[k]
+
+            breaksUB = 0
+            for example in negData:
+                if u.subs({x: example[i]}) > ub[k][i]:
+                    breaksUB = 1
+                    break
+            if breaksUB == 0:
+                del ub[k]
+
+    # for b in generate_binary_expr(x,y):
+    #     k=str(b)
+    #
+    #     for i in range(len(lb[k])):
+    #         breaksLB = 0
+    #         for example in negData:
+    #             if b.subs({x:v1, y: v2})<lb[k][i]:
+    #                 breaksLB=1
+    #                 break
+    #         if breaksLB==1:
+    #             break
+    #     if breaksLB==0:
+    #         del lb[k]
+    #
+    #     breaksUB = 0
+    #     for example in negData:
+    #         for i,(v1, v2) in enumerate(pairs(example)):
+    #             if b.subs({x: v1, y: v2})>ub[k][i]:
+    #                 breaksUB = 1
+    #                 break
+    #         if breaksUB == 1:
+    #             break
+    #     if breaksUB == 0:
+    #         del ub[k]
+    #
+    # for u in generate_unary_exp(x):
+    #     for b in generate_binary_expr(x,y):
+    #         k = str(u)
+    #         k = k.replace('x', '(' + str(b) + ')')
+    #         breaksLB = 0
+    #         for example in negData:
+    #             for i, (v1, v2) in enumerate(pairs(example)):
+    #                 if u.subs({x: b.subs({x: v1, y: v2})}) < lb[k][i]:
+    #                     breaksLB = 1
+    #                     break
+    #             if breaksLB == 1:
+    #                 break
+    #         if breaksLB == 0:
+    #             del lb[k]
+    #
+    #         breaksUB = 0
+    #         for example in negData:
+    #             for i, (v1, v2) in enumerate(pairs(example)):
+    #                 if u.subs({x: b.subs({x: v1, y: v2})}) > ub[k][i]:
+    #                     breaksUB = 1
+    #                     break
+    #             if breaksUB == 1:
+    #                 break
+    #         if breaksUB == 0:
+    #             del ub[k]
+    return lb, ub
 
 
+def create_model(data, bounds):
+    x, y = symbols('x y')
+    cpvars = []
+    for vdict in data['formatTemplate']['list']:
+        # {'high': 10, 'low': 1, 'type': 'dvar'}
+        cpvars.append(intvar(vdict['low'], vdict['high']))
+    cpvars = cpm_array(cpvars)  # make it a CPM/Numpy array
+
+    m = Model()
+    for expr, inst in bounds.items():
+        # print(expr)
+        for (index), (lb, ub) in inst.items():
+            if len(index)==1:
+                e = sympify(expr)
+                f = lambdify(x, e)
+                cpm_e = f(cpvars[index[0]])
+                m += [cpm_e >= lb, cpm_e <= ub]
+            else:
+                e = sympify(expr)
+                f = lambdify([x,y], e)
+                cpm_e = f(cpvars[index[0]], cpvars[index[1]])
+                m += [cpm_e >= lb, cpm_e <= ub]
+
+    unsat_cons = musx(m.constraints)
+    model2 = Model(unsat_cons)
+    print(model2)
+    # model2.solve()
+    # print(model2.status())
+    # cpvars.value()
 
 
 if __name__ == '__main__':
     # constraint_learner()
     # x, y = symbols('x y')
-    # for b in generate_expressions(x,y):
+    # for b in generate_binary_expr(x,y):
     #     print(b,":", b.subs({x:5, y: 10}))
 
     data = json.load(open('instances/type01/instance0.json'))
-    size = len(data['formatTemplate']['list'])
-    inputData = [d['list'] for d in data['tests']]
-    print(inputData[0])
-    lb, ub = constraint_learner(inputData[0])
-    print(lb)
+    posData = np.array([d['list'] for d in data['solutions']])
+    negData = np.array([d['list'] for d in data['nonSolutions']])
+    print("number of solutions: ", len(posData))
+    print("number of non-solutions: ", len(negData))
+    bounds = constraint_learner(posData, posData.shape[1])
+    print(len(bounds))
+    create_model(data, bounds)
+    # print(len(bounds))
+    # lb,ub = filter_negatives(negData, lb, ub)
+    # print(len(lb), len(ub))
