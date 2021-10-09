d4c500d4a7602596df6c57e4e8dea5692a461379
Mohit Kumar <mohitkrg@gmail.com>
2021-10-09 14:08:19 +0200

    integrated filter for trivial bounds in create_model

--- learner.py
+++ learner.py
@@ -8,15 +8,19 @@ import minizinc
 from musx import musx
 from cpmpy.solvers import CPM_ortools
 
-from cpmpy.expressions.variables import _NumVarImpl
-def pow(self, y=2):
-    assert (y==2)
-    return self*self
-_NumVarImpl.__pow__ = pow
+from cpmpy.transformations.flatten_model import get_or_make_var
+
+# from cpmpy.expressions.variables import _NumVarImpl
+# def pow(self, y=2):
+#     assert (y==2)
+#     return self*self
+# _NumVarImpl.__pow__ = pow
+
 
 def pairs(example):
     for [x, y] in it.product(example, repeat=2):
-        yield x,y
+        yield x, y
+
 
 def index_pairs(exampleLen):
     for (i, j) in it.combinations(range(exampleLen), r=2):
@@ -28,7 +32,7 @@ def unary_operators():
         return x % 2
 
     def power(x):
-        return x*x
+        return x * x
 
     def identity(x):
         return x
@@ -36,30 +40,34 @@ def unary_operators():
     for f in [identity, abs]:
         yield f
 
+
 def generate_unary_exp(x):
     for u in unary_operators():
         yield u(x)
 
-def binary_operators(x,y):
+
+def binary_operators(x, y):
     for u in unary_operators():
         yield u(x) + u(y)
         yield u(x) - u(y)
         yield u(y) - u(x)
 
+
 def generate_binary_expr(x, y):
-    for b in binary_operators(x,y):
+    for b in binary_operators(x, y):
         yield b
 
-    for b in binary_operators(x,y):
-        for u in generate_unary_exp(b): #redundancy due to identity
+    for b in binary_operators(x, y):
+        for u in generate_unary_exp(b):  # redundancy due to identity
             yield u
+
 
 def constraint_learner(solutions, n_vars):
     bounds = dict()
-    x, y = symbols('x y')
+    x, y = symbols("x y")
     for u in generate_unary_exp(x):
         k = str(u)
-        bounds[k]=dict()
+        bounds[k] = dict()
         f = lambdify(x, u, "math")
         for i in range(n_vars):
             vals = f(solutions[:, i])
@@ -68,24 +76,25 @@ def constraint_learner(solutions, n_vars):
     for b in generate_binary_expr(x, y):
         k = str(b)
         bounds[k] = dict()
-        f = lambdify([x,y], b, "math")
-        for (i,j) in index_pairs(n_vars):
+        f = lambdify([x, y], b, "math")
+        for (i, j) in index_pairs(n_vars):
             vals = f(solutions[:, i], solutions[:, j])
-            bounds[k][(i,j)] = (min(vals), max(vals))
+            bounds[k][(i, j)] = (min(vals), max(vals))
     return bounds
 
+
 def filter_negatives(negData, lb, ub):
-    x, y = symbols('x y')
+    x, y = symbols("x y")
     for u in generate_unary_exp(x):
-        k=str(u)
+        k = str(u)
 
         for i in range(len(lb[k])):
             breaksLB = 0
             for example in negData:
-                if u.subs({x: example[i]})<lb[k][i]:
-                    breaksLB=1
+                if u.subs({x: example[i]}) < lb[k][i]:
+                    breaksLB = 1
                     break
-            if breaksLB==0:
+            if breaksLB == 0:
                 del lb[k]
 
             breaksUB = 0
@@ -150,27 +159,31 @@ def filter_negatives(negData, lb, ub):
 
 
 def create_model(data, bounds):
-    x, y = symbols('x y')
+    x, y = symbols("x y")
     cpvars = []
-    for vdict in data['formatTemplate']['list']:
+    for vdict in data["formatTemplate"]["list"]:
         # {'high': 10, 'low': 1, 'type': 'dvar'}
-        cpvars.append(intvar(vdict['low'], vdict['high']))
+        cpvars.append(intvar(vdict["low"], vdict["high"]))
     cpvars = cpm_array(cpvars)  # make it a CPM/Numpy array
 
     m = Model()
     for expr, inst in bounds.items():
         # print(expr)
         for (index), (lb, ub) in inst.items():
-            if len(index)==1:
+            if len(index) == 1:
                 e = sympify(expr)
                 f = lambdify(x, e)
                 cpm_e = f(cpvars[index[0]])
-                m += [cpm_e >= lb, cpm_e <= ub]
+                (v, _) = get_or_make_var(cpm_e)
+                if (lb, ub) != (v.lb, v.ub):
+                    m += [cpm_e >= lb, cpm_e <= ub]
             else:
                 e = sympify(expr)
-                f = lambdify([x,y], e)
+                f = lambdify([x, y], e)
                 cpm_e = f(cpvars[index[0]], cpvars[index[1]])
-                m += [cpm_e >= lb, cpm_e <= ub]
+                (v, _) = get_or_make_var(cpm_e)
+                if (lb, ub) != (v.lb, v.ub):
+                    m += [cpm_e >= lb, cpm_e <= ub]
 
     # unsat_cons = musx(m.constraints)
     # model2 = Model(unsat_cons)
@@ -187,10 +200,10 @@ def check_solutions(m, mvars, sols, exp, objectives, verbose=False):
         return 1.0
 
     sats = []
-    for i,sol in enumerate(sols):
+    for i, sol in enumerate(sols):
         m2 = Model([c for c in m.constraints])
-        m2 += (mvars == sol)
-        sat = m2.solve() and exp(sol)==objectives[i]
+        m2 += mvars == sol
+        sat = m2.solve() and exp(sol) == objectives[i]
         sats.append(sat)
 
         if verbose:
@@ -199,15 +212,16 @@ def check_solutions(m, mvars, sols, exp, objectives, verbose=False):
             else:
                 print(f"!!! Sol {sol} does not satisfy after all")
     print(f"{sum(sats)} satisfied out of {len(sats)}")
-    return sum(sats)*100.0 / len(sats)
+    return sum(sats) * 100.0 / len(sats)
+
 
 def check_obective(exp, sols, objectives, verbose=False):
     if len(sols) == 0:
         print("No solutions to check")
         return 1.0
     sats = []
-    for i,sol in enumerate(sols):
-        sat = exp(sol)==objectives[i]
+    for i, sol in enumerate(sols):
+        sat = exp(sol) == objectives[i]
         sats.append(sat)
 
         if verbose:
@@ -216,32 +230,32 @@ def check_obective(exp, sols, objectives, verbose=False):
             else:
                 print(f"!!! Sol {sol} does not satisfy the objective {exp(sol)}")
     print(f"{sum(sats)} objectives satisfied out of {len(sats)}")
-    return sum(sats)*100.0 / len(sats)
+    return sum(sats) * 100.0 / len(sats)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # constraint_learner()
     # x, y = symbols('x y')
     # for b in generate_binary_expr(x,y):
     #     print(b,":", b.subs({x:5, y: 10}))
     args = sys.argv[1:]
     data = json.load(open(f"instances/type0{args[0]}/instance{args[1]}.json"))
-    posData = np.array([d['list'] for d in data['solutions']])
-    posDataObj = np.array([d['objective'] for d in data['solutions']])
-    negData = np.array([d['list'] for d in data['nonSolutions']])
-    negDataObj = np.array([d['objective'] for d in data['nonSolutions']])
+    posData = np.array([d["list"] for d in data["solutions"]])
+    posDataObj = np.array([d["objective"] for d in data["solutions"]])
+    negData = np.array([d["list"] for d in data["nonSolutions"]])
+    negDataObj = np.array([d["objective"] for d in data["nonSolutions"]])
     print("number of solutions: ", len(posData))
     print("number of non-solutions: ", len(negData))
     bounds = constraint_learner(posData, posData.shape[1])
-    numConstr=0
+    numConstr = 0
     for k, v in bounds.items():
         numConstr += len(v)
     print(f"learned {numConstr} constraints from {len(bounds)} different expressions")
 
-    m, mvars=create_model(data, bounds)
-    check_solutions(m, mvars, posData[:100], max, posDataObj[:100])
-    check_solutions(m, mvars, negData[:100], max, negDataObj[:100])
+    m, mvars = create_model(data, bounds)
+    print(f"number of constraints in the model: {len(m.constraints)}")
+    check_solutions(m, mvars, posData[:10], max, posDataObj[:10])
+    check_solutions(m, mvars, negData[:10], max, negDataObj[:10])
     # check_obective(max, negData, negDataObj)
     # print(len(bounds))
     # lb,ub = filter_negatives(negData, lb, ub)
