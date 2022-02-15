ab6b3ecd6762b364f3ecd163f9b58e3130a82519
Mohit Kumar <mohitkrg@gmail.com>
2022-02-15 16:02:36 +0100

    experiments on competition data

--- experiments.py
+++ experiments.py
@@ -391,73 +391,73 @@ def type_level_experiment():
     csvfile.close()
 
 
-def generalized_learning_experiment(types):
+def generalized_learning_experiment(t):
     # csvfile = open(f"type_level_results.csv", "w")
     # pickleVar = {}
-    for t in types:
-        print(f"type{t}")
-        with open(f"type {t:02d}.csv", "w") as csv_file:
-            filewriter = csv.writer(csv_file, delimiter=",")
+    # for t in types:
+    print(f"type{t}")
+    with open(f"type {t:02d}.csv", "w") as csv_file:
+        filewriter = csv.writer(csv_file, delimiter=",")
+        filewriter.writerow(
+            [
+                "type",
+                "instance",
+                "total_constraints",
+                "learned_constraints",
+                "learning_time",
+                "testing_time",
+                "perc_pos",
+                "perc_neg"
+            ]
+        )
+        path = f"instances/type{t:02d}/inst*.json"
+        files = sorted(glob.glob(path))
+        instances = []
+        for file in files:
+            with open(file) as f:
+                instances.append(Instance(int(file.split("/")[-1].split(".")[0][8:]), json.load(f), t))
+        start = time.time()
+        bounding_expressions = learn(instances)
+        learning_time = time.time() - start
+        pickleVar = bounding_expressions
+
+        for instance in instances:
+            # len_pos, len_neg = 0, 0
+            print(f"instance {instance.number}")
+            learned_model, total_constraints = create_gen_model(bounding_expressions, instance)
+            start_test = time.time()
+            # precision, recall = learner.compare_models(learned_model, model_type01(instance), instance)
+            # recall = cc*100/tc
+            # precision = cc*100/lc
+            # print(recall, precision)
+            perc_pos, perc_neg = None, None
+            if instance.pos_data is not None:
+                sols = [list(d[k]) for d in instance.pos_data for k in instance.tensors_dim]
+                perc_pos = learner.check_solutions_fast(
+                    learned_model, instance.cp_vars, sols, max, instance.pos_data_obj
+                )
+                print("perc_pos: ", perc_pos)
+            if instance.neg_data is not None:
+                non_sols = [list(d[k]) for d in instance.neg_data for k in instance.tensors_dim]
+                perc_neg = 100 - learner.check_solutions_fast(
+                    learned_model, instance.cp_vars, non_sols, max, instance.neg_data_obj
+                )
+                print("perc_neg: ", perc_neg)
             filewriter.writerow(
                 [
-                    "type",
-                    "instance",
-                    "total_constraints",
-                    "learned_constraints",
-                    "learning_time",
-                    "testing_time",
-                    "perc_pos",
-                    "perc_neg"
+                    t,
+                    instance.number,
+                    total_constraints,
+                    len(learned_model.constraints),
+                    learning_time,
+                    time.time() - start_test,
+                    perc_pos,
+                    perc_neg,
+                    # recall,
+                    # precision,
                 ]
             )
-            path = f"instances/type{t:02d}/inst*.json"
-            files = sorted(glob.glob(path))
-            instances = []
-            for file in files:
-                with open(file) as f:
-                    instances.append(Instance(int(file.split("/")[-1].split(".")[0][8:]), json.load(f), t))
-            start = time.time()
-            bounding_expressions = learn(instances)
-            learning_time = time.time() - start
-            pickleVar = bounding_expressions
-
-            for instance in instances:
-                # len_pos, len_neg = 0, 0
-                print(f"instance {instance.number}")
-                learned_model, total_constraints = create_gen_model(bounding_expressions, instance)
-                start_test = time.time()
-                # precision, recall = learner.compare_models(learned_model, model_type01(instance), instance)
-                # recall = cc*100/tc
-                # precision = cc*100/lc
-                # print(recall, precision)
-                perc_pos, perc_neg = None, None
-                if instance.pos_data is not None:
-                    sols = [list(d[k]) for d in instance.pos_data for k in instance.tensors_dim]
-                    perc_pos = learner.check_solutions_fast(
-                        learned_model, instance.cp_vars, sols, max, instance.pos_data_obj
-                    )
-                    print("perc_pos: ", perc_pos)
-                if instance.neg_data is not None:
-                    non_sols = [list(d[k]) for d in instance.neg_data for k in instance.tensors_dim]
-                    perc_neg = 100 - learner.check_solutions_fast(
-                        learned_model, instance.cp_vars, non_sols, max, instance.neg_data_obj
-                    )
-                    print("perc_neg: ", perc_neg)
-                filewriter.writerow(
-                    [
-                        t,
-                        instance.number,
-                        total_constraints,
-                        len(learned_model.constraints),
-                        learning_time,
-                        time.time() - start_test,
-                        perc_pos,
-                        perc_neg,
-                        # recall,
-                        # precision,
-                    ]
-                )
-        pickle.dump(pickleVar, open(f"type{t:02d}_bound_expressions.pickle", "wb"))
+    pickle.dump(pickleVar, open(f"type{t:02d}_bound_expressions.pickle", "wb"))
     # csvfile.close()
 
 
