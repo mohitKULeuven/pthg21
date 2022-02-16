85dbdd5f4df476c1c2e07389a6ac5cfa7ea9a3bb
Tias Guns <tias.guns@kuleuven.be>
2022-02-16 15:09:24 +0100

    types: use inst.check() for easity

--- instances/type04.py
+++ instances/type04.py
@@ -76,19 +76,6 @@ if __name__ == "__main__":
     m = model_type04(inst)
     print(m)
 
-
     # sanity check ground truth
-    sols, non_sols = [], []
-    if inst.pos_data is not None:
-        sols = [np.hstack([list(d[k].flatten()) for k in inst.tensors_dim]) for d in inst.pos_data]
-        perc_pos = learner.check_solutions_fast(
-            m, inst.cp_vars, sols, max, inst.pos_data_obj
-        )
-        print("perc_pos: ", perc_pos)
-    if inst.neg_data is not None:
-        non_sols = [np.hstack([list(d[k].flatten()) for k in inst.tensors_dim]) for d in inst.neg_data]
-        perc_neg = 100 - learner.check_solutions_fast(
-            m, inst.cp_vars, non_sols, max, inst.neg_data_obj
-        )
-        print("perc_neg: ", perc_neg)
+    print("pos/neg accuracy", inst.check(m))
 
